21a9b76155c27fc2283d999b06415265faa0981c
Pablo <placdeveloper@gmail.com>
2023-04-09 19:30:04 -0300

    configurando microservico de cartoes de cliente no api gateway

--- apiGateway/src/main/java/br/com/pabllo007/config/RouteConfig.java
+++ apiGateway/src/main/java/br/com/pabllo007/config/RouteConfig.java
@@ -15,6 +15,7 @@ public class RouteConfig {
         return builder
                 .routes()
                 .route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+                .route(r -> r.path("/cartoes/**").uri("lb://mscartoes"))
                 .build();
     }
 }
