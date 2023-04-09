99572db5b437590c451bba5a5c9720dbc009fc94
Pablo <placdeveloper@gmail.com>
2023-04-09 17:32:54 -0300

    Criação do projeto API Gateway

---
+++ apiGateway/src/main/java/br/com/pabllo007/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package br.com.pabllo007.apigateway;
+
+import br.com.pabllo007.config.RouteConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@Import(RouteConfig.class)
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ apiGateway/src/test/java/br/com/pabllo007/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pabllo007.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
